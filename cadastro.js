a1ed7b42e648ab0c3d715754f5788f0992c23acf
devAna <caldas.ana9@gmail.com>
2022-11-27 09:47:26 -0300

    Adicionando funcionalidade de mostar mensagem

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,12 @@
+
+// Pegando o email e cadastrando na plataforma
+
 function singUp(email){
     return email;
 }
 
-// Pegando o email e cadastrando na plataforma
+
+function mostraMensagem(){
+    return "Bem vindo ao sistema";
+}
+
